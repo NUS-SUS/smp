3b9e571c8e8ec5c9c8be230440455cc5b6c838b1
haliminhashim <haliminhashim@gmail.com>
2022-08-17 19:03:00 +0800

    basic auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,6 @@ export type AmplifyDependentResourcesAttributes = {
         "smpe824f8a0": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
