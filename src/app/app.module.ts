ddb3bb06fa0f7744bd1eb7f4e3b6cd80c7731265
haliminhashim <haliminhashim@gmail.com>
2022-08-18 22:11:33 +0800

    Added Feedback and Discover Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiscoverComponent } from './components/discover/discover.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscoverComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    RouterModule.forRoot([
+      {path: 'discover', component: DiscoverComponent},
+      {path: 'feedback', component: FeedbackComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
