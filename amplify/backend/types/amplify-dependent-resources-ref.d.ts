ffc08c3b04f1b0973af683fd06561d8638f43188
haliminhashim <haliminhashim@gmail.com>
2022-08-18 17:23:41 +0800

    Remove auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "smpe824f8a0": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
