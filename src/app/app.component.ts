720e19f5d3676fb686eecd7f750a320424666ac4
haliminhashim <haliminhashim@gmail.com>
2022-08-18 17:40:30 +0800

    Add Basic Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 @Component({
   selector: 'app-root',
