72ef5ca07596d4e5e95700cd34a740215ccc1fa3
haliminhashim <haliminhashim@gmail.com>
2022-08-18 17:48:02 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 @Component({
   selector: 'app-root',
