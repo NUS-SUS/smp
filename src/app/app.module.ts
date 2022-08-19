0949476c69c722d0766b6c19f7f0ca23abae9403
haliminhashim <haliminhashim@gmail.com>
2022-08-19 13:03:13 +0800

    Update Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DiscoverComponent } from './components/discover/discover.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { HomeComponent } from './components/home/home.component';
+import { CampaignComponent } from './components/campaign/campaign.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiscoverComponent,
     FeedbackComponent,
-    HomeComponent
+    HomeComponent,
+    CampaignComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,8 +30,11 @@ import { HomeComponent } from './components/home/home.component';
     RouterModule.forRoot([
       {path: 'discover', component: DiscoverComponent},
       {path: 'feedback', component: FeedbackComponent},
-      {path: 'home', component: HomeComponent}
-    ])
+      {path: 'home', component: HomeComponent},
+      {path: 'campaign', component: CampaignComponent},
+      {path: 'profile', component: ProfileComponent}
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
